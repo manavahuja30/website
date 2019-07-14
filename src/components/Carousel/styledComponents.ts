9d156cc1ecfac625ded31b4f198b6b4193f01ad0
momsse <momssesystem@gmail.com>
2019-07-14 23:49:04 +0200

    :art: carousel

---
+++ src/components/Carousel/styledComponents.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const RADIUS = '0.5rem';
+
+export const Picture = styled.img`
+  border-radius: ${RADIUS};
+`;
+
+export const CarouselInner = styled.div`
+  border-radius: ${RADIUS};
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import React, { ReactElement } from 'react';
 
+import Carousel from '../components/Carousel';
 import IndexLayout from '../layouts';
 
 const IndexPage = (): ReactElement => (
   <IndexLayout>
-    <h1>Acceuil</h1>
+    <Carousel
+      id="carousel1"
+      caption="L'île aux singes"
+      pictures={[
+        require('./__fixtures__/samples/L_ile_aux_singes/1.jpg'),
+        require('./__fixtures__/samples/L_ile_aux_singes/2.jpg'),
+        require('./__fixtures__/samples/L_ile_aux_singes/3.jpg'),
+        require('./__fixtures__/samples/L_ile_aux_singes/4.jpg'),
+        require('./__fixtures__/samples/L_ile_aux_singes/5.jpg'),
+        require('./__fixtures__/samples/L_ile_aux_singes/6.jpg'),
+      ]}
+    />
   </IndexLayout>
 );
 
